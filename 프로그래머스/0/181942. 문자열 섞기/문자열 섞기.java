be890f412451889151466be17f29b9ff2d635ffb
sehee moon <snowball9820@gmail.com>
2024-02-26 22:01:46 +0900

    [level 0] Title: 문자열 섞기, Time: 14.44 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,10 +1,8 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        
-        for(int i=0; i<str1.length();i++){
-            answer+=str1.substring(i,i+1);
-            answer+=str2.substring(i,i+1);
+        for(int i=0; i<str1.length(); i++){
+            answer=answer+str1.charAt(i)+str2.charAt(i);
         }
         return answer;
     }
