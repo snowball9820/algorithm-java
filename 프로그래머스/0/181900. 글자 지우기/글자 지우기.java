051aaf6db81463c68a778f44a582605b21c447b4
sehee moon <snowball9820@gmail.com>
2024-03-04 21:26:31 +0900

    [level 0] Title: 글자 지우기, Time: 2.12 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        String[] str=my_string.split("");
+        
+        for(int i=0; i<indices.length;i++){
+            str[indices[i]]="";
+            
+        }
+        for(String x:str){
+            answer+=x;
+        }
+        return answer;
+    }
+}
